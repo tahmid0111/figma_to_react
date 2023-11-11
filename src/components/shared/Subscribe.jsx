77d35cd4bf1cbaad6b78f447f1ef81dde9d88112
tahmid-emam <0.1tahmidemam@gmail.com>
2023-11-11 20:33:51 +0600

    subscribe component created

--- src/components/shared/Subscribe.jsx
+++ src/components/shared/Subscribe.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Subscribe = () => {
     return (
         <>
-            
+            <div className="subscribe-area">
+                <h1>edwdw</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim velit ad aut illum alias perspiciatis quaerat sapiente commodi nihil reiciendis nobis eius, quasi culpa tempore cupiditate deleniti beatae autem incidunt delectus dolor voluptatibus quibusdam. Explicabo placeat veniam unde dolor a eligendi, expedita deleniti vel officiis natus neque. Consectetur, sunt sed autem dolorum voluptates necessitatibus saepe enim quae tenetur nisi aut temporibus totam. Assumenda labore sapiente rerum odit dignissimos vero aut corporis quasi minima qui quos debitis sed totam, perferendis a facere error praesentium. Magnam, iste molestias sapiente impedit assumenda fuga ratione et, voluptatibus aliquid, accusantium error necessitatibus. Quo, eius quasi!</p>
+            </div>
         </>
     );
 };
