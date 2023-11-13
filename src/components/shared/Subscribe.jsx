b1c4cde97af6933647fd2ba3c33393c61ce0c007
tahmid-emam <0.1tahmidemam@gmail.com>
2023-11-13 21:47:12 +0600

    moved to another file

--- src/components/shared/Subscribe.jsx
+++ src/components/shared/Subscribe.jsx
@@ -4,10 +4,14 @@ import { FaBeer } from 'react-icons/fa';
 const Subscribe = () => {
     return (
         <>
-            <div className="subscribe-area">
-                <h1>edwdw</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim velit ad aut illum alias perspiciatis quaerat sapiente commodi nihil reiciendis nobis eius, quasi culpa tempore cupiditate deleniti beatae autem incidunt delectus dolor voluptatibus quibusdam. Explicabo placeat veniam unde dolor a eligendi, expedita deleniti vel officiis natus neque. Consectetur, sunt sed autem dolorum voluptates necessitatibus saepe enim quae tenetur nisi aut temporibus totam. Assumenda labore sapiente rerum odit dignissimos vero aut corporis quasi minima qui quos debitis sed totam, perferendis a facere error praesentium. Magnam, iste molestias sapiente impedit assumenda fuga ratione et, voluptatibus aliquid, accusantium error necessitatibus. Quo, eius quasi!</p>
-                <FaBeer />
+            <div className="subscribe-area container mx-auto text-center my-8 py-8">
+                <p className='text-xl text-orange-500'>SUBSCRIBE</p>
+                <h2 className='text-3xl font-bold w-2/6 ml-[35%] pt-4 pb-2'>Subscribe To Get The Latest News about Us</h2>
+                <p className='text-base pb-8'>Please Drop Your Email Below To Get Daily Update About What We Do</p>
+                <form>
+                    <input type="text" placeholder='Enter Your Email Address' className='input bg-warning relative' />
+                    <button className='subscribe-btn px-5 py-2 bg-orange-500 text-white rounded-lg absolute'>Subscribe</button>
+                </form>
             </div>
         </>
     );
