91af0fb3147d6123b5356d89fab3a52599e80444
tahmid-emam <0.1tahmidemam@gmail.com>
2023-11-19 22:33:16 +0600

    team page done almost

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -4,7 +4,10 @@ import Layout from '../Layout/Layout'
 const Service = () => {
   return (
     <Layout>
-      
+        <div className='container mx-auto'>
+            <h1 className='text-xl font-bold py-5 text-orange-500'>OUR TEAM MEMBER</h1>
+            <p className='text-2xl w-[60%] pb-5'>Check our awesome team members</p>
+        </div>
     </Layout>
   )
 }
